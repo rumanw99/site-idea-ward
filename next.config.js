a02e86670fd29a49d1b5fe8a33e1c352efe1ab24
rumanw99 <99048499+rumanw99@users.noreply.github.com>
2025-05-27 03:38:00 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'www.semabrands.com',
-      'www.voila.digital',
-      'www.sohu.com',
-      'cdn-icons-png.flaticon.com',
-      'images.unsplash.com',
-      'randomuser.me',
-    ],
+    domains: ['images.unsplash.com', 'randomuser.me'],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
